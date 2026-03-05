fb1d7e72a3c71025cfa189bcb53a94c154ce557d
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:27:18 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:26:36.272Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T14:27:19.357Z
+Commit: Fix race condition in async operations
