011e60bd0c72022b7adff1657263e0dcb5e7ebd4
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:30:41 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:30:13.569Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T14:30:42.333Z
+Commit: Fix broken links in documentation
