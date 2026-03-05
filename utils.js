d7432eab856db240439e5ec08c010d3d1fd53c6b
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:31:22 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:31:08.419Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T14:31:24.146Z
+Commit: Fix memory leak in event handlers
