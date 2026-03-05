6163e01b96f0000e0e3351a5e5f435d5b28f4a7a
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:35:39 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:35:39.168Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T14:35:40.228Z
+Commit: Fix broken links in documentation
