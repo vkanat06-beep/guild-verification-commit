bedd759db58d926a9d1155f552068b683a6e23f4
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:34:19 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:33:57.403Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T14:34:20.880Z
+Commit: Fix broken links in documentation
