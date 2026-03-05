35b486ce77020dc0ae263605d70ad3b84b345990
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:33:13 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:32:35.089Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T14:33:14.544Z
+Commit: Fix CSS styling bug on mobile devices
