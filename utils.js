bc6649f9d83bbf5c9f860b192d19fc5f6807cf69
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:23:15 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:22:54.813Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T14:23:16.930Z
+Commit: Fix CSS styling bug on mobile devices
