831f649ef0eb073ef2286ac887dd2b7567517d86
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:31:43 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:31:24.146Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T14:31:45.013Z
+Commit: Fix broken links in documentation
