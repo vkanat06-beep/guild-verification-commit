b7b62c0f6e62dd25d6e8fcf26e3a5ffd5830b735
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:36:11 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:36:03.600Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T14:36:13.129Z
+Commit: Implement user profile customization
