95a941c545c1c04c97ef04234386e0b28a16aa6e
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:23:43 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:23:41.849Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T14:23:44.441Z
+Commit: Fix CSS styling bug on mobile devices
