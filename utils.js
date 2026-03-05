a4b11a018ee3a5842a1829034c47877383ab7007
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:20:33 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:20:25.349Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T14:20:34.852Z
+Commit: Resolve null pointer exception in user service
