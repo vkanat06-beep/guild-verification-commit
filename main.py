b7d0383723c550d35c0cf460d033a1f8ae79d279
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:36:44 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:36:29.064Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T14:36:45.592Z
+Commit: Fix broken links in documentation
