35cb7a4d0aba45b288b1434d6320d355edf1a687
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:36:12 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:36:01.737Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T14:36:14.179Z
+Commit: Fix broken links in documentation
