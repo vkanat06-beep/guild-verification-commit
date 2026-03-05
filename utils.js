edd4232256d47045632bd1b9b25627e39c8209c0
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:33:15 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:33:14.544Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T14:33:16.770Z
+Commit: Add export functionality for reports
