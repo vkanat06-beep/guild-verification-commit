b61aade2af707669b916ec8db20b7f1ffde88d0f
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:30:53 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:30:32.846Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T14:30:54.648Z
+Commit: Fix memory leak in event handlers
