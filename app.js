2c951896db967439ab8c09ca5c9b7e05efcc753f
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:24:17 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:24:15.484Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T14:24:18.303Z
+Commit: Fix broken links in documentation
