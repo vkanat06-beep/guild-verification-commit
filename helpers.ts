bdaff53f52197c52d1870b70bc322391125dac2a
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:36:25 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:36:25.278Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T14:36:27.142Z
+Commit: Patch security vulnerability in authentication
