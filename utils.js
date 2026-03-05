a404bdeacb749a24260a35ea49727be68e27beb1
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:23:33 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:23:16.930Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T14:23:34.823Z
+Commit: Patch API endpoint error handling
