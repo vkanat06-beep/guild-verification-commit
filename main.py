e90783c1d45225ac04ea806bd3736d94517c958c
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:31:09 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:31:07.329Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T14:31:10.468Z
+Commit: Resolve timezone issues in date handling
