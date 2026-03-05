a2834dcc6d5957273aadf68d444903c5685e9934
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:36:28 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:36:08.476Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T14:36:29.919Z
+Commit: Add export functionality for reports
