3a805b8ebb8740dae82eef78fe804dc1d3424178
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:22:45 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:21:32.759Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T14:22:46.436Z
+Commit: Patch API endpoint error handling
