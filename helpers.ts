970318713a88ceccb936460ae2c04dde8a98f827
vkanat06-beep <vkanat06@gmail.com>
2026-03-05 17:26:35 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:26:34.058Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T14:26:36.272Z
+Commit: Fix broken links in documentation
